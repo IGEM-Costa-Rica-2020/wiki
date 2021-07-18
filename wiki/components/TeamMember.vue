57e7aec0a19541f53da0eda7b128920d2edbf824
Gustavo Segura <yo@gustavosegura.com>
2021-07-18 02:53:26 -0600

    team members demo page

---
+++ wiki/components/TeamMember.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <!-- <div v-b-hover="handleHover"> -->
+      <!-- <img
+        v-if="isHovered"
+        :src="require(`@/assets/images/bacteria/${memberData.id}.png`)"
+        class="card-img-top"
+        :alt="memberData.name"
+      />
+
+      <img
+        v-else
+        :src="require(`@/assets/images/profile/${memberData.id}.png`)"
+        class="card-img-top"
+        :alt="memberData.name"
+      /> -->
+      <img
+        :src="require(`@/assets/images/profile/${memberData.id}.png`)"
+        class="card-img-top"
+        :alt="memberData.name"
+      />
+    <!-- </div> -->
+
+    <div class="rectangle">
+      <div class="lula-stokes">{{ memberData.name }}</div>
+      <div class="cmo">{{ memberData.career }}</div>
+    </div>
+    <div class="mask"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { memberData: {} },
+  data() {
+    return {
+      isHovered: false,
+    }
+  },
+  methods: {
+    handleHover(hovered) {
+      this.isHovered = hovered
+    },
+  },
+  computed: {
+    getImgUrl() {
+      var images = require.context('../assets/images/profile', false, /\.png$/)
+      return images('./' + 'jose' + '.png')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.mask {
+  height: 8px;
+  /* width: 349px; */
+  border-radius: 0 0 2px 2px;
+  background-color: #69c183;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.1);
+}
+.cmo {
+  height: 24px;
+  /* width: 277px; */
+  color: #5c5c5c;
+  font-family: Graphik;
+  font-size: 12px;
+  letter-spacing: 0;
+  line-height: 24px;
+}
+.lula-stokes {
+  height: 15px;
+  /* width: 277px; */
+  color: #27262d;
+  font-family: Graphik;
+  font-size: 14px;
+  font-weight: 600;
+  letter-spacing: -0.3px;
+  line-height: 15px;
+}
+.rectangle {
+  /* height: 83px; */
+  /* width: 349px; */
+  border-radius: 0 0 2px 2px;
+  background-color: #ffffff;
+  /* box-shadow: 0 0 20px 0 rgba(45,41,38,0.24); */
+}
+</style>
---
+++ wiki/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+<team-member
+  v-for="member in tmdata"
+  :key="member.id"
+  :memberData="member"
+  />
+  <!-- <Tutorial/> -->
+
+
+
+</div>
+  
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TeamMember from '../components/TeamMember.vue'
+import {TeamMembersData} from "../assets/data/TeamMembers.js"
+export default Vue.extend({
+  data(){
+    return{
+      tmdata:TeamMembersData
+    }
+  }
+})
+</script>
+
