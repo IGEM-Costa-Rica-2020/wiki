36cd3c02e2461dabd1887aa1ad5674e8ae178138
Gustavo Segura <yo@gustavosegura.com>
2021-10-20 19:56:35 -0600

    human

---
+++ texto_barras.js
@@ -0,0 +1,304 @@
+
+COLLABORATION
+
+
+[{
+                            title: "Collaboration Initiatives",
+                            content: "In this section we present collaborations in which we not only took part, but proposed or helped organize in some way."
+                        },
+                        {
+                            title: "Meetups & Events",
+                            content: "Here you can find the meetups or public events we attended and learn about the way they impacted our work"
+                        },
+                        {
+                            title: "Interviews",
+                            content: "Interviews are a great way to engage between teams, specifically when creating shared content such as the initiatives presented next."
+                        },
+                        {
+                            title: "Public Engagement",
+                            content: "If one wishes to really have impact, engaging with the public is a key feature for any and every project; to do it collectively is even better"
+    
+                        }
+                    ]
+
+DESCRIPTION
+
+"circuit": [{
+                            title: "Introduction",
+                            content: "Our project began with an objective in mind: expanding the boundaries of engineered organisms, specifically targeting their release. The design and implementation of a genetic suicide circuit is the start of this journey."
+                        },
+                        {
+                            title: "Function",
+                            content: "Our circuit’s behavior is directly related to four separate modules. Each of these exhibits a specific function in order to fulfill a determined purpose. Here we propose a coordinated behavior; nonetheless they can also be used separately. "
+                        },
+                        {
+                            title: "Architecture and theoretical basis",
+                            content: "As for any well designed genetic circuit we took great care and consideration for the selection of the different parts and proteins included in our circuit. In this case, architecture is of high relevance, since it’s the key to allow the behavior and function we described earlier. This is even truer for our counter circuit, which we conceptualized as a state machine and, as such, defines each state by specific DNA configurations and requires a specific input to change state."
+                        },
+                        {
+                            title: "Characteristics",
+                            content: "To showcase some of the best qualities of our circuit, we handpicked some of its characteristics of interest, most of them brought forward by our user interviews. We also included a troubleshooting section in order to aid the optimization of our circuit based on each user’s specific needs. "
+                        },
+                        {
+                            title: "Functional Animation",
+                            content: "To better understand the behavior of our circuit we have developed an interactive animation in which each module can be activated and explored as their different steps unfold."
+                        },
+                        {
+                            title: "Extras",
+                            content: "This section contains additional modules along with variations for the ones presented within our main circuit. Their intention is to improve functionality and expand the utilization possibilities."
+
+                        },
+                        {
+                            title: "Comparison to other counter circuits",
+                            content: "To further demonstrate the novelty and capacity of our counter circuit we present a comparison with previously developed counters, highlighting their characteristics of interest."
+                        },
+             						{
+                            title: "References",
+                            content: ""
+                        
+                        }
+                    ]
+"framework": [{
+                            title: "Introduction",
+                            content: "How did the framework come to be? who are our users?"
+
+                        },
+                        {
+                            title: "Project Description",
+                            content: "What is our conceptual framework?"
+
+                        },
+                        {
+                            title: "Foundational Concepts",
+                            content: "The framework is supported by three key concepts: sequential logic, abstraction and modularity"
+                        },
+                        {
+                            title: "Framework's architecture",
+                            content: "How our guide is organized"
+                        },
+                        {
+                            title: "Biological State Machines",
+                            content: "The section Biological state machines encompasses an expansion of how a circuit can be expressed as a state machine and the characteristics such a system has."
+
+                        },
+                        {
+                            title: "Description",
+                            content: "The description phase entails the conceptualization of biological objects and how they interact with one another to carry out complex processes. Here we provide thorough definitions for each concept."
+
+                        },
+                        {
+                            title: "Execution",
+                            content: "The second phase -the execution phase- entails the union of the concepts previously defined, both the structural parts and the interactions between them, and sequential logic to describe the design, and model the changes of states of a genetic circuit. Here we propose the complete workflows for this process."
+
+                        },
+              					{
+                            title: "Software Framework",
+                            content: "The implementation of the conceptual framework as a guide for software design"
+                        },
+                        {
+                            title: "Applied Conceptual Framework: Developing the Software Framework",
+                            content: "As mentioned before, the software framework is a direct implementation of the conceptual framework, by applying the concepts described and the coupling of them with sequential logic as computational concepts and a workflow."
+                        }
+                    ]
+
+IMPLEMENTATION
+
+"circuit": [{
+                            title: "Overview",
+                            content: "Here we mention a brief introduction about the perspective of the project and its potential relevance of use in the real world."
+
+                        },
+                        {
+                            title: "Applications",
+                            content: "Areas of interest that we choose to describe and demonstrate the utility of our system regarding experts' feedback and improvement ideas for past iGEM proposals based on the implementation of our project."
+
+                        },
+                        {
+                            title: "Safety Implications",
+                            content: "Safety aspects that are important to consider in order to prevent risks and ensure security for the use of our system"
+                        },
+                        {
+                            title: "Regulations",
+                            content: "If a project is meant to be implemented in the real world, then regulations and policy need to be taken into account"
+                        },
+                        {
+                            title: "End Users & Stakeholders",
+                            content: "Potential end users and stakeholders for our system based on their interest and power to influence our project"
+                        },
+                        {
+                            title: "What's Next",
+                            content: "Possible impact of our project for the scientific community and further applications of interest"
+                        }
+                    ]
+"framework": [{
+                            title: "Overview",
+                            content: "Here we mention a brief introduction about the perspective of the project and its potential relevance of use in the real world."
+
+                        },
+                        {
+                            title: "Applications",
+                            content: "Areas of interest that we choose to describe and demonstrate the utility of our system regarding experts' feedback and improvement ideas for past iGEM proposals based on the implementation of our project."
+
+                        },
+                        {
+                            title: "Safety Implications",
+                            content: "Safety aspects that are important to consider in order to prevent risks and ensure security for the use of our system"
+                        },
+                        {
+                            title: "End Users & Stakeholders",
+                            content: "Potential end users and stakeholders for our system based on their interest and power to influence our project"
+                        },
+                        {
+                            title: "What's Next",
+                            content: "Possible impact of our project for the scientific community and further applications of interest"
+                        }
+                    ]
+
+EDUCATION/COMMUNICATION (usan exactamente los mismos)
+
+[{
+                            title: "Educational Content & Target Audience",
+                            content: ""
+                        },
+                        {
+                            title: "LabxChange",
+                            content: ""
+                        },
+                        {
+                            title: "Virtual Labs & Interactives",
+                            content: ""
+                        },
+                        {
+                            title: "Design Process",
+                            content: ""
+                        },
+                        {
+                            title: "Additional Information",
+                            content: ""
+                        }
+                    ]
+
+SOFTWARE 
+[{
+                            title: "Overview",
+                            content: "A tool for exploring the design space of synthetic gene circuits' architectures"
+                        },
+                        {
+                            title: "Scope",
+                            content: "What we set to achieve with our tool"
+                        },
+                        {
+                            title: "Software Description",
+                            content: "Here we present a detailed and comprehensible exposition of the actual software, how to expand it, and its users guide"
+                        },
+                        {
+                            title: "Results",
+                            content: "An analysis on the generated circuits to evaluate the software's performance, explore the generated circuits, and analyze different parameters' effect on the results."
+                        }
+                    ]
+MEASUREMENT
+[{
+                            title: "Promoter Strenght",
+                            content: ""
+                        }
+                    ]
+MODEL
+[{
+                            title: "ODE's",
+                            content: "We adapted a serine recombination model to our system in order to study and predict kinetics behaviour of species involved in the process and how that would affect our system. We also used it as a tool to optimize parameters for the lab."
+                        },
+                        {
+                            title: "Gillespie Algorithm",
+                            content: "Randomness plays an important role in biology, especially in microscopic events which are determined by random clashes of molecules that produce a chemical reaction. By applying the gillespie algorithm, we adapted our differential equations to randomize our process."
+                        },
+                        {
+                            title: "Markov Model",
+                            content: "We would like to know how likely it is that our genetic counter works, for that we adopted a Markov model approach in order to have an idea in terms of probabilities."
+                        },
+                        {
+                            title: "Maximum Population",
+                            content: "Motivated by the possible applications of our circuit, we developed this model and tool in order to gain insight into how a whole population of bacteria with a specific purpose would behave once it is released with our genetic counter inside them."
+                        },
+                        {
+                            title: "Decision Tree",
+                            content: "Here we present an original idea developed by our own, which marries the concept of sequential logic and construction of genetic circuits. We hope this work can serve as a basis to find hidden patterns on decision trees."
+                        },
+                        {
+                            title: "Metabolic Burden",
+                            content: "In this section we assess the impact on how metabolic burden would affect the functionality of our genetic counter, building upon the work of Austin UTexas iGEM team 2019."
+                        }
+                    ]
+
+PROOF OF CONCEPT no tiene apparently
+
+WET LAB
+
+[{
+                            title: "Overview",
+                            content: "The general objective of executing lab work is to experimentally characterize a circuit."
+                        },
+                        {
+                            title: "Assembly Design",
+                            content: "Since we had limited base pairs for circuit design, our gene fragments were carefully planned to be efficiently constructed and to allow versatile characterization."
+                        },
+                        {
+                            title: "Experimental characterization design",
+                            content: "The basis of our characterization are fluorescent proteins."
+                        },
+                        {
+                            title: "Further Characterization",
+                            content: "Several other characterization targets were proposed towards the end of our time in the competition. We invite future teams to address them in their own projects. "
+                        },
+                        {
+                            title: "Protocols",
+                            content: "Here are the protocols that are going to be used to characterize the genetic constructs described in parts. These protocols consist of a number of steps followed to accomplish each step from strain construct to characterization. Each specific modification made on a specific protocol will be specified on notebook."
+                        },
+                        {
+                            title: "Notebook",
+                            content: "We kept track of our progress in the lab through a notebook where detailed information was recorded. "
+                        },
+                        {
+                            title: "Parts",
+                            content: "For our project we generated two composite parts and a basic part, which are detailed in this section."
+                        },
+ 												{
+                            title: "Results",
+                            content: "Due to unexpected circumstances beyond our control we were unable to perform our devised characterization; however, we decided to carry out some in silico characterizations for our proteins instead."
+                        }
+                    ]
+
+HUMAN PRACTICES
+
+
+[{
+                            title: "Timeline",
+                            content: "Below is a timeline that summarizes our progress in the competition. To find the integration of each of the aspects mentioned below, please refer to the rest of the Human Practices sections."
+                        },
+                        {
+                            title: "Ethical & Biosafety Considerations",
+                            content: "We have centered our project on values such as integrity, responsibility, balance, accountability, commitment, perseverance and innovation, and around the intention of minimizing its potential risks and the ones of its further uses. We also committed to raising awareness of these concerns and involving perspectives beyond our expertise."
+                        },
+                        {
+                            title: "Experimental characterization design",
+                            content: "The basis of our characterization are fluorescent proteins."
+                        },
+                        {
+                            title: "Potential Users Feedback",
+                            content: "We consulted several experts, in the quality of potential users, for a realistic evaluation of our tool’s value and its efficiency at targeting their needs."
+                        },
+                        {
+                            title: "Experts Feedback",
+                            content: "Multiple meetings with professionals, from a wide range of expertise inside and outside synthetic biology, were held to receive technical advice, validation and in some cases improvement pointers regarding our project."
+                        },
+                        {
+                            title: "Shipping & Policy",
+                            content: "In order to avoid misinformation from taking part in unjustified regulations, slow bureaucracy and other obstacles, we felt the call to be agents for change, as funnells for the voicing of others experiences and as advisors for generations to come."
+                        },
+                        {
+                            title: "Public Engagement",
+                            content: "People must feel personally involved with biotechnological development in order to build bilateral relationships and to truly involve non-scientists by encouraging their participation, making them feel listened to and giving them tools for the development of an informed opinion. "
+                        },
+                        {
+                            title: "Integrated Human Practices",
+                            content: "To validate our project as a tool we focused on the search for all possible profiles that could provide a real technical approach to ensure our project is useful and functional from the raw to the cooked version of Chronobacter."
+                    ]
